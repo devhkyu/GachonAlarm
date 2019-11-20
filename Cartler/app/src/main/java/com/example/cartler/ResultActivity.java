a37ad77fccb6e1d20130d33043f3fac77ca8a9e5
Hyungkyu Choi <fabric93@naver.com>
2019-11-20 16:02:52 +0900

    Add movement variable in controlView
    Direction, Angle, Power

--- Cartler/app/src/main/java/com/example/cartler/ResultActivity.java
+++ Cartler/app/src/main/java/com/example/cartler/ResultActivity.java
@@ -4,10 +4,17 @@ import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import com.zerokol.views.joystickView.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultActivity extends AppCompatActivity {
+    private TextView angleTextView;
+    private TextView powerTextView;
+    private TextView directionTextView;
+    private JoystickView joystick;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +28,50 @@ public class ResultActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 setContentView(R.layout.control);
+                angleTextView = (TextView) findViewById(R.id.angleTextView);
+                powerTextView = (TextView) findViewById(R.id.powerTextView);
+                directionTextView = (TextView) findViewById(R.id.directionTextView);
+
+                //Referencing also other views
+                joystick = (JoystickView) findViewById(R.id.joystickView);
+
+                //Event listener that always returns the variation of the angle in degrees, motion power in percentage and direction of movement
+                joystick.setOnJoystickMoveListener(new JoystickView.OnJoystickMoveListener() {
+                    @Override
+                    public void onValueChanged(int angle, int power, int direction) {
+                        // TODO Auto-generated method stub
+                        angleTextView.setText(" " + String.valueOf(angle) + "°");
+                        powerTextView.setText(" " + String.valueOf(power) + "%");
+                        switch (direction) {
+                            case JoystickView.FRONT:
+                                directionTextView.setText(R.string.front_lab);
+                                break;
+                            case JoystickView.FRONT_RIGHT:
+                                directionTextView.setText(R.string.front_right_lab);
+                                break;
+                            case JoystickView.RIGHT:
+                                directionTextView.setText(R.string.right_lab);
+                                break;
+                            case JoystickView.RIGHT_BOTTOM:
+                                directionTextView.setText(R.string.right_bottom_lab);
+                                break;
+                            case JoystickView.BOTTOM:
+                                directionTextView.setText(R.string.bottom_lab);
+                                break;
+                            case JoystickView.BOTTOM_LEFT:
+                                directionTextView.setText(R.string.bottom_left_lab);
+                                break;
+                            case JoystickView.LEFT:
+                                directionTextView.setText(R.string.left_lab);
+                                break;
+                            case JoystickView.LEFT_FRONT:
+                                directionTextView.setText(R.string.left_front_lab);
+                                break;
+                            default:
+                                directionTextView.setText(R.string.center_lab);
+                        }
+                    }
+                }, JoystickView.DEFAULT_LOOP_INTERVAL);
             }
         });
     }
--- Cartler/app/src/main/java/com/example/cartler/ui/home/HomeFragment.java
+++ Cartler/app/src/main/java/com/example/cartler/ui/home/HomeFragment.java
@@ -11,21 +11,18 @@ import android.widget.Button;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProviders;
 import com.example.cartler.R;
 import com.example.cartler.ResultActivity;
+
 import java.util.Calendar;
 
 import static android.content.Context.MODE_PRIVATE;
 
 public class HomeFragment extends Fragment {
-    private HomeViewModel homeViewModel;
     private Context context;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+                             final ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
         context = container.getContext();
         Calendar calendar = Calendar.getInstance();
