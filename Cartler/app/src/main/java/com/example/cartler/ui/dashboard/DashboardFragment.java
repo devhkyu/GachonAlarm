f4d63981f1c03979a979d2e03c7d2b7a534ab9cf
Hyungkyu Choi <fabric93@naver.com>
2019-11-22 20:25:41 +0900

    Redesign Home, Setting fragment

--- Cartler/app/src/main/java/com/example/cartler/ui/dashboard/DashboardFragment.java
+++ Cartler/app/src/main/java/com/example/cartler/ui/dashboard/DashboardFragment.java
@@ -1,20 +1,14 @@
 package com.example.cartler.ui.dashboard;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import com.example.cartler.R;
 import app.akexorcist.bluetotohspp.library.BluetoothSPP;
-import app.akexorcist.bluetotohspp.library.BluetoothState;
-import app.akexorcist.bluetotohspp.library.DeviceList;
 
 public class DashboardFragment extends Fragment {
 
@@ -26,70 +20,6 @@ public class DashboardFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         root = inflater.inflate(R.layout.fragment_dashboard, container, false);
         context = container.getContext();
-
-        bt = new BluetoothSPP(context);
-        if (!bt.isBluetoothAvailable()) {
-            Toast.makeText(context, "Bluetooth is not available", Toast.LENGTH_SHORT).show();
-        }
-        bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() { //데이터 수신
-            public void onDataReceived(byte[] data, String message) {
-                Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        bt.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() { //연결됐을 때
-            public void onDeviceConnected(String name, String address) {
-                Toast.makeText(context, "Connected to " + name + "\n" + address, Toast.LENGTH_SHORT).show();
-            }
-
-            public void onDeviceDisconnected() { //연결해제
-                Toast.makeText(context, "Connection lost", Toast.LENGTH_SHORT).show();
-            }
-
-            public void onDeviceConnectionFailed() { //연결실패
-                Toast.makeText(context, "Unable to connect", Toast.LENGTH_SHORT).show();
-            }
-        });
-        Button btnConnect = root.findViewById(R.id.btnConnect); //연결시도
-        btnConnect.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (bt.getServiceState() == BluetoothState.STATE_CONNECTED) {
-                    bt.disconnect();
-                } else {
-                    Intent intent = new Intent(context, DeviceList.class);
-                    startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
-                }
-            }
-        });
         return root;
-    }
-    public void onDestroy(){
-        super.onDestroy();
-        bt.stopService();
-    }
-    public void onStart(){
-        super.onStart();
-    }
-    public void setup() {
-        Button btnSend = root.findViewById(R.id.btnSend); //데이터 전송
-        btnSend.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                bt.send("Text", true);
-            }
-        });
-    }
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
-            if (resultCode == Activity.RESULT_OK)
-                bt.connect(data);
-        } else if (requestCode == BluetoothState.REQUEST_ENABLE_BT) {
-            if (resultCode == Activity.RESULT_OK) {
-                bt.setupService();
-                bt.startService(BluetoothState.DEVICE_OTHER);
-                setup();
-            } else {
-                Toast.makeText(context, "Bluetooth was not enabled.", Toast.LENGTH_SHORT).show();
-            }
-        }
     }
 }
